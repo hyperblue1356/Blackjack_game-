c4d851952cfed58849371a4135d016b7f6c0491a
hyperblue1356 <69151435+hyperblue1356@users.noreply.github.com>
2021-03-26 12:25:45 +0400

    Update

--- challenge_calculator.py
+++ challenge_calculator.py
@@ -1,53 +1,63 @@
+
+
+# Write a GUI program to create a simple calculator
+# layout that looks like the screenshot.
+#
+# Try to be as Pythonic as possible - it's ok if you
+# end up writing repeated Button and Grid statements,
+# but consider using lists and a for loop.
+#
+# There is no need to store the buttons in variables.
+#
+# As an optional extra, refer to the documentation to
+# work out how to use minsize() to prevent your window
+# from being shrunk so that the widgets vanish from view.
+#
+# Hint: You may want to use the widgets .winfo_height() and
+# winfo_width() methods, in which case you should know that
+# they will not return the correct results unless the window
+# has been forced to draw the widgets by calling its .update()
+# method first.
+#
+# If you are using Windows you will probably find that the
+# width is already constrained and can't be resized too small.
+# The height will still need to be constrained, though.
+
 try:
     import tkinter
-except ImportError:  # python 2
+except ImportError: # python 2
     import Tkinter as tkinter
 
-from tkinter import *
 
-import os
+keys = [[('WINDRUNNER', 1), ('CE', 1)],
+        [('7', 1), ('8', 1), ('9', 1), ('+', 1)],
+        [('4', 1), ('5', 1), ('6', 1), ('-', 1)],
+        [('1', 1), ('2', 1), ('3', 1), ('*', 1)],
+        [('0', 1), ('=', 1), ('/', 1)],
+        ]
+mainWindowPadding = 8
 
 mainWindow = tkinter.Tk()
-
-mainWindow.title("Grid Demo")
+mainWindow.title("Calculator")
 mainWindow.geometry('640x480-8-200')
-mainWindow['padx'] = 8
+mainWindow['padx'] = mainWindowPadding
 
-label = tkinter.Label(mainWindow, text="Calculator")
-label.grid(row=0, column=0, columnspan=3)
-
-mainWindow.columnconfigure(0, weight=100)
-mainWindow.columnconfigure(1, weight=1)
-mainWindow.columnconfigure(2, weight=1000)
-mainWindow.columnconfigure(3, weight=600)
-mainWindow.columnconfigure(4, weight=1000)
-
-mainWindow.rowconfigure(0, weight=1)
-mainWindow.rowconfigure(1, weight=10)
-mainWindow.rowconfigure(2, weight=1)
-mainWindow.rowconfigure(3, weight=3)
-mainWindow.rowconfigure(4, weight=3)
-
-# frame for the radio buttons
-optionFrame = tkinter.LabelFrame(mainWindow, text="File Details")
-optionFrame.grid(row=1, column=2, sticky='ne')
-
-rbValue = tkinter.IntVar()
-rbValue.set(2)
-
-radio1 = tkinter.Radiobutton(optionFrame, text="1", value=1, variable=rbValue, bd=0)
-radio2 = tkinter.Radiobutton(optionFrame, text="2", value=2, variable=rbValue)
-radio3 = tkinter.Radiobutton(optionFrame, text="3", value=3, variable=rbValue)
-radio1.grid(row=0, column=0, sticky='w')
-radio2.grid(row=0, column=1, sticky='w')
-radio3.grid(row=0, column=3, sticky='w')
-
-# Widget to display result
-resultLabel = tkinter.Label(mainWindow)
-resultLabel.grid(row=0, column=0, sticky='nw')
 result = tkinter.Entry(mainWindow)
-result.grid(row=0, column=0, sticky='sw')
-mainWindow['padx'] = 8
+result.grid(row=0, column=0, sticky='nsew')
+
+keyPad = tkinter.Frame(mainWindow)
+keyPad.grid(row=1, column=0, sticky='nsew')
+
+row = 0
+for keyRow in keys:
+    col = 0
+    for key in keyRow:
+        tkinter.Button(keyPad, text=key[0]).grid(row=row, column=col, columnspan=key[1], sticky=tkinter.E + tkinter.W)
+        col += key[1]
+    row += 1
+
+mainWindow.update()
+mainWindow.minsize(keyPad.winfo_width() + mainWindowPadding, result.winfo_height() + keyPad.winfo_height())
+mainWindow.maxsize(keyPad.winfo_width() + mainWindowPadding, result.winfo_height() + keyPad.winfo_height())
 
 mainWindow.mainloop()
-
